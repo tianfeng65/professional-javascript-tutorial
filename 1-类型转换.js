a2bd99dd431c3c1f17e27e21a7641c9a5e8ae0f3
tianfeng6 <tianfeng6@xiaomi.com>
2020-09-22 16:07:50 +0800

    类型转换-Number()

--- 1-类型转换.js
+++ 1-类型转换.js
@@ -11,3 +11,36 @@ Boolean(NaN)     // false
 Boolean(null)      // false
 Boolean(undefined)     // false
 Boolean(false)    // false
+
+//2、其他类型 =>数值类型
+/**
+ * 有3个函数可以将非数值转换为数值：Number()、parseInt()和parseFloat()。
+ * 2.1、首先看Number()转型函数，可用于任何数据类型。(一元加操作符与Number()函数遵循相同的转换规则。)
+ */
+const obj = {
+    a: 1,
+    valueOf() {
+        console.log("执行了valueOf方法")
+        return {}
+    },
+    toString() {
+        console.log("执行了toString方法")
+        return "123"
+    }
+}
+
+const arr = [
+    Number(true),   // 1
+    Number(false),  // 0 
+    Number(null),   // 0
+    Number(undefined),  // NaN
+    Number(""), // 0
+    Number("012.34"),   // 12.34
+    Number("-12.34"),   // -12.34
+    Number("0o70"),     // 56
+    Number("0x16"),     // 22
+    +"0x16",    // 22
+    Number(obj) // 123 在其中先执行了valueOf方法，然后执行了toString方法
+]
+
+console.log(arr)
